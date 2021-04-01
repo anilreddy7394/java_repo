e7adf45146910169f3f31545fbf6fe91df7e5f63
anilreddy7394 <81523825+anilreddy7394@users.noreply.github.com>
2021-04-01 20:59:17 +0530

    Update App.java

--- src/main/java/com/flipkart/App.java
+++ src/main/java/com/flipkart/App.java
@@ -1,13 +1,10 @@
 package com.flipkart;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+package hello;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
 }
